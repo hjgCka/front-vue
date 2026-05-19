beca924f6ef670617c7f51bd472b71dccc4c0210
hu_jiangang <hu_jiangang@ctg.com.cn>
2026-05-19 22:07:09 +0800

    更新hello.ts

--- ts_demo/src/hello.ts
+++ ts_demo/src/hello.ts
@@ -1,11 +1,5 @@
 const hello : string = "Hello World!"
 console.log(hello)
 
-var a : number = 5
-console.log(a)
-
-const b = 'Banana'
-console.log(b)
-
 const apple = 'Apple'
 console.log(apple.toUpperCase())
