251e008a021c5eb71855a0e01b591faa5d0ac871
hjg <computer_cka@163.com>
2026-05-21 23:06:38 +0800

    更新文件。

--- ts_demo/src/hello.ts
+++ ts_demo/src/hello.ts
@@ -3,3 +3,11 @@ console.log(hello)
 
 const apple = 'Apple'
 console.log(apple.toUpperCase())
+
+// 先定义类型，在去用它。那么返回值为void的话，其实不会管返回的是什么。
+// 但是不能拿着返回值去做判断。
+type logFunc = () => void
+const f1:logFunc = function() {
+    return 66
+}
+console.log(f1())
