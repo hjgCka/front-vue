7250a4dbe0bf1bc131eb0957b4d558ce636ee86e
hjg <computer_cka@163.com>
2026-07-02 23:19:37 +0800

    更新vue3-study。

--- 6_vue3/vue3-study/src/App.vue
+++ 6_vue3/vue3-study/src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="app">
-    <h1>你好阿！</h1>
-  </div>
+    <Person/>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import Person from './components/Person.vue';
 
+export default {
+    components: {Person}
+}
 </script>
 
 <style scoped>
-  .app {
-    background-color: #ddd;
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-    padding: 20px;
-  }
+
 </style>
--- 6_vue3/vue3-study/src/components/Person.vue
+++ 6_vue3/vue3-study/src/components/Person.vue
@@ -2,19 +2,91 @@
     <div class="person">
         <h2>姓名：{{ name }}</h2>
         <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
     </div>
 </template>
 
+<!-- 不带语法糖的写法 -->
+<!--
 <script lang="ts">
     export default {
         name: 'Person',
+        /*
         data() {
             return {
                 name: '张三',
                 age: 18,
                 tel: '18822221111'
             }
+        },
+        methods: {
+            showTel() {
+                alert(this.tel)
+            },
+            changeName() {
+                this.name = 'zhang-san';
+            },
+            changeAge() {
+                this.age += 1;
+            }
+        },
+        */
+        // 执行时机比beforeCreate更早，该方法内this是undefined
+        setup() {
+            // 此时3个数据不是响应式
+            let name = '张三'
+            let age = 19
+            let tel = '13822221111'
+
+            function changeName() {
+                name = 'zhang-san'
+            }
+            function changeAge() {
+                age += 1
+            }
+            function showTel() {
+                alert(tel)
+            }
+
+            //vue2的配置选项 data()和 methods可以与 setup同时存在，并且起效
+            //vue2中的data或methods可以用到setup内定义的数据和方法，通过this引用
+            //但是setup内无法使用vue2配置的数据和方法，因为setup执行时机比beforeCreate还早
+
+            // setup如果返回函数，也就是渲染函数（此时可用箭头函数），那么就会覆盖模板的内容
+            // return () => '哈哈'
+            return {
+                name, age,
+                changeName, changeAge, showTel
+            }
         }
+    }
+</script>
+-->
+
+<!-- 这个标签可用来配置组件名称，但是可以不写，如果不写的话使用vue文件名作为组件名。 -->
+<script lang="ts">
+    export default {
+        name: 'Person'
+    }
+</script>
+
+<!-- 带语法糖的写法，相当于里面内容都在setup函数内，且返回了对象。
+ 需要上方的不带setup的script标签。 -->
+<script setup lang="ts">
+    let name = '张三'
+    let age = 19
+    let tel = '13822221111'
+
+    function changeName() {
+        name = 'zhang-san'
+    }
+    function changeAge() {
+        age += 1
+    }
+    function showTel() {
+        alert(tel)
     }
 </script>
 
@@ -24,5 +96,8 @@
         box-shadow: 0 0 10px;
         border-radius: 10px;
         padding: 20px;
+    }
+    button {
+        margin: 0px 5px;
     }
 </style>
