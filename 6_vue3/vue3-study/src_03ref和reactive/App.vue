2ba2469f5489630d8b59176305f291671f2a5943
hjg <computer_cka@163.com>
2026-07-21 22:33:08 +0800

    更新文件。

--- 6_vue3/vue3-study/src_03ref和reactive/App.vue
+++ 6_vue3/vue3-study/src_03ref和reactive/App.vue
@@ -1,13 +1,15 @@
 <template>
     <Person/>
+    <Car/>
 </template>
 
 <script lang="ts">
+import Car from './components/Car.vue';
 import Person from './components/Person.vue';
 
 // 局部注册组件
 export default {
-    components: {Person}
+    components: {Person, Car}
 }
 </script>
 
