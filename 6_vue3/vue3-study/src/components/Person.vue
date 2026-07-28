735d7868b9f9402231dc1e776789ca67eaf074a0
hjg <computer_cka@163.com>
2026-07-28 22:55:03 +0800

    更新文件。

--- 6_vue3/vue3-study/src/components/Person.vue
+++ 6_vue3/vue3-study/src/components/Person.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="person">
-        
+        <h2>姓名：{{ p1.name }}</h2>
+        <h2>年龄：{{ p1.age }}</h2>
+
+        <ul>
+            <li v-for="item in list" :key="item.id">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
 
 <script setup lang="ts">
+    import {ref} from 'vue'
     import {type PersonInter, type Persons} from '@/types'
     let person:PersonInter = {id:'1213', name: '张三', age: 60}
 
@@ -15,9 +21,12 @@
     // ]
     let personList:Persons = [
         {id:'1213', name: '张三', age: 60},
-        {id:'1213', name: '张三', age: 60},
-        {id:'1213', name: '张三', age: 60}
+        {id:'1214', name: '李四', age: 60},
+        {id:'1215', name: '王五', age: 60}
     ]
+
+    let p1 = ref(person)
+    let list = ref(personList)
 </script>
 
 <style scoped>
